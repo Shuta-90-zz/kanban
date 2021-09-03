c2d58f8ce34b84617de13cfb9ccca315f71eaf64
Shuta <shuta.ogura0921@gmail.com>
2021-09-03 16:00:11 +0900

    4th chapter

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,5 @@
 class TopController < ApplicationController
   def index
-    @lists = List.where(user:current_user).order('created_at asc')
+    @lists = List.where(user: current_user).order('created_at asc')
   end
 end
